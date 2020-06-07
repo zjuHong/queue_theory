0df025f7ebfcc7843238265b0f95c68705a004b9
zjuHong <47885322+zjuHong@users.noreply.github.com>
2020-06-07 14:42:41 +0800

    Update get_poisson.c

--- get_poisson.c
+++ get_poisson.c
@@ -1,5 +1,6 @@
 // https://www.cnblogs.com/liam-ji/p/11679026.html
 // https://www.cnblogs.com/liam-ji/p/11623024.html
+// 生成符合泊松分布的随机数
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -12,15 +13,16 @@ int main()
 　　int p;
 　　int i=0;
 　　for(i=0;i<20;i++){
-　　    p=possion();
+　　    p=possion(i);
         printf("%d ",p);
 　　}
 　　printf("\n");
 　　return 0;
 }
-int possion()
+//输入参数：泊松分布的λ
+int possion(int lambda)
 {
-　　int lambda=320,k=0;
+　　int k=0;
 　　long double p=1.0;
 　　long double l=exp(-lambda);
 　　double u=U_Random();
